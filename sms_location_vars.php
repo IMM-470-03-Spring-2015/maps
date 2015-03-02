f9384fe58488219711ab484a115993393e2a038f
Mark D. Thompson <thomesoni@gmail.com>
2015-03-01 19:27:38 -0500

    updated

--- sms_location_vars.php
+++ sms_location_vars.php
@@ -6,24 +6,22 @@
     $zip = $_REQUEST['FromZip'];
     $country = $_REQUEST['FromCountry'];
     $msg = $_REQUEST['Body'];
-   
+
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
     // get the exact time of the response
     $t = time();
-
     // set the timezone
     date_default_timezone_set('America/New_York');
 
     // open a file ponter to write our data
     $fp = fopen("data.txt", "a");
-
     fwrite($fp, $from . "," . $city . "," . $state . "," . $zip . "," . $country . "," . date("c", $t) . "\n");
     
     // close out file pointer
     fclose($fp);
 ?>
 <Response>
-    <Message>Thanks! Here's your phone's exchange location: <?php echo $city . "," . $state . "," . $zip . "," . $country; ?></Message>
+    <Message>Thanks! Here's your phone's location: <?php echo $city . ", " . $state . ", " . $zip . ", " . $country; ?></Message>
 </Response>
--- sms_map_data.php
+++ sms_map_data.php
@@ -1,29 +1,28 @@
 <?php
     // we can get the number of the sender using the 'Fromn' request value
     $from = $_REQUEST['From'];
-    $city = $_REQUEST['FromCity'];
-    $state = $_REQUEST['FromState'];
-    $zip = $_REQUEST['FromZip'];
-    $country = $_REQUEST['FromCountry'];
     $msg = $_REQUEST['Body'];
-   
+
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-
-    // get the exact time of the response
-    $t = time();
-
-    // set the timezone
-    date_default_timezone_set('America/New_York');
+    
+    // break up the message into an array for cleaning
+    $msg_arr = explode(",",$msg);
+    // remove any extra spaces
+    for($i=0; $i<count($msg_arr); $i++){
+        $msg_arr[$i] = trim($msg_arr[$i]);
+    }
+    
+    // put it back together as coma separated
+    $msg = implode(",",$msg_arr);
 
     // open a file ponter to write our data
-    $fp = fopen("data.txt", "a");
-
-    fwrite($fp, $from . "," . $city . "," . $state . "," . $zip . "," . $country . "," . date("c", $t) . "\n");
+    $fp = fopen("twilio_data.txt", "a");
+    fwrite($fp,  $msg . "\n");
     
     // close out file pointer
     fclose($fp);
 ?>
 <Response>
-    <Message>Thanks! Here's your phone's exchange location: <?php echo $city . "," . $state . "," . $zip . "," . $country; ?></Message>
+    <Message>Thanks for the incident report!</Message>
 </Response>
